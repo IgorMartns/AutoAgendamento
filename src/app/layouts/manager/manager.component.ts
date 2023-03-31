bd16b72f5c8f6cae9c3d1e1756477590fa6dc352
IgorMartns <igor.inacio.martins@outlook.com>
2023-03-30 22:16:25 -0300

    Tela convênios feita

--- src/app/layouts/manager/manager.component.ts
+++ src/app/layouts/manager/manager.component.ts
@@ -10,14 +10,44 @@ import { map, Observable, single, startWith } from 'rxjs';
 export class ManagerComponent {
   logged = false
   myControl = new FormControl('')
+  myPlano : any 
+  planos = []
   convenios: any = [
-    { nome: "Portomed", sigla: "PM" },
-    { nome: "Amil", sigla: "AML" },
-    { nome: "Amais", sigla: "AM" },
-    { nome: "Unimed", sigla: "UM" },
-    { nome: "Bradesco", sigla: "BRD" },
+    {
+      nome: "Portomed", sigla: "PM", planos: [
+        { nome: "Diamante 10", sigla: "D10" },
+        { nome: "Diamente 20", sigla: "D20" },
+        { nome: "Diamente 30", sigla: "D30" },
+      ]
+    },
+    {
+      nome: "Amil", sigla: "AML", planos: [
+        { nome: "Amil Fácil S40 SP Enf", sigla: "AF40" },
+        { nome: "Amil Fácil S80 SP Apto", sigla: "AF80" },
+      ]
+    },
+    {
+      nome: "Amais", sigla: "AM", planos: [
+        { nome: "Amais SP 100", sigla: "AM100" },
+        { nome: "Amail 500", sigla: "AM500" },
+        { nome: "SulAmerica Exato", sigla: "SAE" },
+        { nome: "GNDI ADVANCE 600", sigla: "GNDI" },
+      ]
+    },
+    {
+      nome: "Unimed", sigla: "UM", planos: [
+        { nome: "Unilivre", sigla: "UNI" },
+      ]
+    },
+    {
+      nome: "Bradesco", sigla: "BRD", planos: [
+        { nome: "Nacional Flex", sigla: "NF" }
+      ]
+    },
   ]
+
   conveniosFilter: Observable<string[]>
+  isValid: boolean = true
   constructor() {
     this.conveniosFilter = this.myControl.valueChanges.pipe(
       startWith(''),
@@ -25,12 +55,11 @@ export class ManagerComponent {
     )
   }
 
-  _filter(value:string): string[] {
+  _filter(value: string): string[] {
     const filterValue = value.toLowerCase()
-    return this.convenios.filter(convenio => convenio.nome.toLowerCase().includes(filterValue))
-  }
-
-  changeSelect(item) {
-    console.log(item);
+    var item = this.convenios.filter(convenio => convenio.nome.toLowerCase().includes(filterValue))
+    this.planos = item[0].planos 
+    this.myPlano = ''
+    return item
   }
 }
